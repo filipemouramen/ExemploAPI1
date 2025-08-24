6316c2e1c019f96385788c73a3d108c68993ce73
Filipe <filipemouramen@gmail.com>
2025-08-24 20:41:12 -0300

    API 2

--- ExemploAPI1/Controllers/PessoaController.cs
+++ ExemploAPI1/Controllers/PessoaController.cs
@@ -29,5 +29,42 @@ namespace ExemploAPI1.Controllers
                 IMC = imc
             });
         }
+
+        [HttpGet("consulta-tabela-imc/{imc}")]
+        public IActionResult ConsultaTabelaImc(double imc)
+        {
+            string descricao;
+
+            if (imc < 18.5)
+            {
+                descricao = "Abaixo do peso";
+            }
+            else if (imc >= 18.5 && imc < 24.9)
+            {
+                descricao = "Peso normal";
+            }
+            else if (imc >= 25 && imc < 29.9)
+            {
+                descricao = "Sobrepeso";
+            }
+            else if (imc >= 30 && imc < 34.9)
+            {
+                descricao = "Obesidade grau I";
+            }
+            else if (imc >= 35 && imc < 39.9)
+            {
+                descricao = "Obesidade grau II";
+            }
+            else
+            {
+                descricao = "Obesidade graus III e IV";
+            }
+
+            return Ok(new
+            {
+                IMC = imc,
+                Descricao = descricao
+            });
+        }
     }
 }
